991b4937a19fce9975bcec4923958ed4f0927104
Roberto Espinoza <50055949+Robertorc83@users.noreply.github.com>
2023-01-09 14:45:13 -0500

    Refactoring useMediaQuery

--- app/hooks/useMediaQuery.ts
+++ app/hooks/useMediaQuery.ts
@@ -5,7 +5,6 @@ export function useMediaQuery(query: string) {
 
   useEffect(() => {
     const media = window.matchMedia(query);
-    console.log("media",media)
     if (media.matches !== matches) {
       setMatches(media.matches);
     }
