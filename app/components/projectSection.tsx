c585e1378256dcf9ec1246d35281724bc9d74755
Roberto Espinoza <50055949+Robertorc83@users.noreply.github.com>
2023-02-23 10:39:50 -0500

    Refactored for small screen size 1.3

--- app/components/projectSection.tsx
+++ app/components/projectSection.tsx
@@ -12,11 +12,11 @@ function ProjectSection({techStack, portrait, solution}: ProjectSectionProps) {
         </div>
         <div className="w-full flex flex-col justify-center mt-10">
             <h4 className="animate-text bg-gradient-to-r from-amber-200 via-red-400 to-orange-400 bg-clip-text text-transparent font-monoton text-lg tracking-widest">Tech Stack:</h4>
-            <p className="text-white font-abril px-20 mt-4">{techStack}</p>
+            <p className="text-white font-abril px-5 md:px-20 mt-4">{techStack}</p>
         </div>
         <div className="w-full flex flex-col items-center mt-5">
             <h4 className="animate-text bg-gradient-to-r from-amber-200 via-red-400 to-orange-400 bg-clip-text text-transparent font-monoton text-lg tracking-widest">Solution:</h4>
-            <p className="text-white font-abril px-20 mt-4">{solution}</p>
+            <p className="text-white font-abril px-5 md:px-20 mt-4">{solution}</p>
         </div>
     </div>
   )
